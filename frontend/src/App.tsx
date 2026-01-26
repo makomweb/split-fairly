8a7173df938adc21588608468585f280463b8f06
Martin Komischke <martin.komischke@outlook.com>
2026-01-26 13:16:11 +0100

    minor adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function App() {
   return (
     <div style={{fontFamily: 'system-ui, sans-serif', padding: 20}}>
-      <h1>Fair Split (Vite + React + TS)</h1>
-      <p>Edit frontend/src/App.tsx and see HMR updates.</p>
+      <h1>Project Skeleton (Vite + React + TS)</h1>
+      <p>Edit frontend/src/App.tsx and see HMR updates. 🚀🚀🚀</p>
     </div>
   )
 }
