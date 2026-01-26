9e6e64767a7855f7cf88862fcd9c4be8dac11ec2
Martin Komischke <martin.komischke@outlook.com>
2026-01-26 14:47:10 +0100

    Clean SPA setup: redirect to Vite in dev, serve build in prod

--- backend/src/Controller/SpaController.php
+++ backend/src/Controller/SpaController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SpaController extends AbstractController
@@ -23,37 +23,19 @@ class SpaController extends AbstractController
     public function index(): Response
     {
         if ($this->environment === 'dev') {
-            // Dev mode: fetch from Vite dev server and inject toolbar
-            // Use Docker network hostname (npm-dev service) from within container
-            $viteUrl = 'http://npm-dev:5173/';
-            
-            $httpClient = HttpClient::create();
-            try {
-                $response = $httpClient->request('GET', $viteUrl);
-                $content = $response->getContent();
-            } catch (\Exception $e) {
-                throw new \RuntimeException("Vite dev server not available at {$viteUrl}: " . $e->getMessage());
-            }
-
-            return new Response(
-                $content,
-                Response::HTTP_OK,
-                ['Content-Type' => 'text/html; charset=utf-8']
-            );
+            return new RedirectResponse('http://localhost:5173');
         }
 
-        // Prod mode: serve built static HTML
+        // Production: serve built HTML
         $buildIndex = $this->projectDir . '/public/build/index.html';
         if (!file_exists($buildIndex)) {
             throw new \RuntimeException("SPA build not found at {$buildIndex}");
         }
 
-        $content = file_get_contents($buildIndex);
-        return $this->render('spa.html.twig', [
-            'content' => $content,
-        ], new Response(null, Response::HTTP_OK, [
-            'Content-Type' => 'text/html; charset=utf-8',
-        ]));
+        return new Response(
+            file_get_contents($buildIndex),
+            Response::HTTP_OK,
+            ['Content-Type' => 'text/html; charset=utf-8']
+        );
     }
 }
-
