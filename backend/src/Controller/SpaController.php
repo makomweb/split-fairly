0f4d6a3c3a88c608e900f5c57c0aae6976c900ca
Martin Komischke <martin.komischke@outlook.com>
2026-01-26 11:57:09 +0100

    minor adjustments

--- backend/src/Controller/SpaController.php
+++ backend/src/Controller/SpaController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class SpaController
 {
