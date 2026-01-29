e93d4ed6fadf8a6c3d66740480a0f061244d84bd
Martin Komischke <martin.komischke@outlook.com>
2026-01-29 08:37:22 +0100

    simplify expense tracker class

--- backend/src/SplitFairly/ExpenseTracker.php
+++ backend/src/SplitFairly/ExpenseTracker.php
@@ -5,34 +5,24 @@ declare(strict_types=1);
 namespace App\SplitFairly;
 
 use App\SplitFairly\DTO\Expense;
-use Psr\Log\LoggerInterface;
 
 final readonly class ExpenseTracker
 {
     public function __construct(
-        private readonly CurrentUserInterface $currentUser,
         private readonly EventStoreInterface $eventStore,
         private readonly NormalizerInterface $normalizer,
-        private readonly LoggerInterface $logger,
     ) {
     }
 
     public function track(Expense $expense): void
     {
-        $this->eventStore->persist(
-            event: new Event(
-                subjectType: 'expense',
-                subjectId: $expense->getId()->toRfc4122(),
-                eventType: 'tracked',
-                payload: $this->normalizer->toArray($expense, ['id'])
-            ),
-            dontCommit: false
+        $event = new Event(
+            subjectType: (new \ReflectionClass($expense))->getShortName(),
+            subjectId: $expense->getId()->toRfc4122(),
+            eventType: 'tracked',
+            payload: $this->normalizer->toArray($expense, ['id'])
         );
 
-        $this->logger->info(sprintf(
-            '💰 Expense tracked: "%s" spent %s',
-            $this->currentUser->getEmail(),
-            $expense->price,
-        ), ['expense' => $expense]);
+        $this->eventStore->persist(event: $event, dontCommit: false);
     }
 }
